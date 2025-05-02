94bd46425857196ac6e060404a62b780eb7b42ff
nassim <nassimbzr78@gmail.com>
2025-05-02 17:33:33 +0200

    update logo mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,7 +72,7 @@ export default function Navbar() {
             className='lg:flex lg:ml-14 lg:gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50'>
             <li className='mb-6 hidden max-lg:block'>
               <Link to="/">
-                <img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-36' />
+                <img src="./logo2.png" alt="logo" className='w-36' />
               </Link>
             </li>
             <li className='max-lg:border-b max-lg:py-3 px-3'>
